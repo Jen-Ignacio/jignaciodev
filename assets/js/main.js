beecbb1c4152f5911cf4206ed9919df7de4b99b5
Jiggy <jignacio2204@gmail.com>
2025-02-15 02:30:45 -0500

    version 1 of site

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+let now = new Date();
+let time = document.querySelector('.time');
+
+if(now.getHours() < 12){
+    time.textContent = 'Morning';
+}
+
+if(now.getHours() >= 12 && now.getHours() <= 17){
+    time.textContent = 'Afternoon';
+}
+
+if(now.getHours() > 16){
+    time.textContent = 'Evening';
+}
+
+let pageBodyElement = document.querySelector('body');
+let pageType = pageBodyElement.getAttribute('id');
+
+let html = document.querySelector('html');
+
+if(pageType === 'minimalist'){
+    html.style.scrollPaddingTop = '63px';
+}
+
+if(pageType === 'classic'){
+    html.style.scrollPaddingTop = '76px';
+}
+
+const header = document.querySelector('header');
+const headerHeight = header.offsetHeight;
+const headerPosition = header.getBoundingClientRect().bottom + window.scrollY;
+const offsetPosition = headerPosition - headerHeight;
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    });
+  });
+
+
+const ClassicBtn = document.getElementById('classicPageBtn');
+const MinimalistBtn = document.getElementById('minimalistPageBtn');
+
+if(ClassicBtn){
+    ClassicBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.location.href = 'http://127.0.0.1:5500/index.html';
+    })
+}
+
+if(MinimalistBtn){
+    MinimalistBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.location.href = 'http://127.0.0.1:5500/minimalist.html';
+    })
+}
